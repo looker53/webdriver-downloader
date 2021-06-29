0d4f072adb4a9dfec87afec4a03161accb759c22
yuze <looker53@sina.com>
2021-06-29 16:43:31 +0800

    update the dirname to save the drivers

--- driverloader/config.py
+++ driverloader/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 DRIVER_DIR = 'executor'
-DRIVER_PATH = Path(__file__).parent.resolve() / DRIVER_DIR
+DRIVER_PATH = Path.cwd() / DRIVER_DIR
 if not DRIVER_PATH.exists():
     DRIVER_PATH.mkdir()
